52b16791fc40b93e22ab075c67367735934991ca
Gabriela Bravo Faúndez <gbravo.visual@gmail.com>
2017-05-18 15:40:04 -0400

    Ejercicio enunciado terminado.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
 var pictures = ["assets/img/a1.jpg","assets/img/a2.jpg", "assets/img/a3.jpg", "assets/img/a4.jpg"];
+
+var contenedor = document.getElementsByClassName('otras-imagenes')[0];
+
+//Se recorre el arreglo:
+pictures.forEach(function(f){
+	var img = document.createElement('img'); //Creo un elemento imagen
+	img.setAttribute("src", f); //Le doy el source o fuente de las imagenes
+	img.setAttribute("class","fotos-pandas"); //Le doy una clase
+	contenedor.appendChild(img); //Lo inserto dentro del contenedor creado en html	
+})
